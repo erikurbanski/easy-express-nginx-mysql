b92700f52894e8eb8f85a88754bfa154f32f4f5a
Erik Urbanski <erikurbanski@gmail.com>
2023-12-14 12:30:53 -0300

    Criando BD pela aplicação.

--- node/index.js
+++ node/index.js
@@ -11,25 +11,42 @@ const config = {
 };
 
 const connection = mysql.createConnection(config);
+connection.connect(() => {
+  createTable();
+});
+
+function createTable() {
+  const createTable = `CREATE TABLE IF NOT EXISTS people(
+      id int AUTO_INCREMENT, 
+      name varchar(255) NOT NULL, 
+      PRIMARY KEY (id)
+    )`;
+    connection.query(createTable, (error) => {
+      if (error) {
+        return console.error(error.message);
+      }
+      console.log('Tudo criado!');
+    });
+}
 
 app.get('/', (req, res) => {
-  // res.send('<h1>Full Cycle Rocks!</h1><br>');
-
-  const sql = `INSERT INTO people(name) VALUES('Erik Urbanski')`;
-  connection.query(sql);
-
-  connection.query(`SELECT * FROM people`, (error, results) => {
-    if (error) {
-      return console.error(error.message);
-    }
-    const data = JSON.parse(JSON.stringify(results));
-    let html = '';
-    data.forEach(function(value) {
-      html = html + value.name + '<br/>';
+  setTimeout(function() {
+    const sql = `INSERT INTO people(name) VALUES('Erik Urbanski')`;
+    connection.query(sql);
+  
+    connection.query(`SELECT * FROM people`, (error, results) => {
+      if (error) {
+        return console.error(error.message);
+      }
+      const data = JSON.parse(JSON.stringify(results));
+      let html = '';
+      data.forEach(function(value) {
+        html = html + value.name + '<br/>';
+      });
+  
+      res.send('<h1>Full Cycle Rocks!</h1><br />' + html);
     });
-
-    res.send('<h1>Full Cycle Rocks!</h1><br />' + html);
-  });
+  }, 1000);
 });
 
 app.listen(port, () => {
